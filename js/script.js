4371d477afa4e9a0e261a48a5d9bff679b75d321
lorna nashipae <kimanilorna@yahoo.com>
2018-08-10 12:44:07 +0300

    This commit is to add some few modifications to my script file

--- js/script.js
+++ js/script.js
@@ -4,13 +4,19 @@ function input(){
   var side2=parseInt(prompt("Enter the value of the second side"));
   var side3=parseInt(prompt("Enter the value of the third side"));
 
+
   if(side1===side2&&side2===side3){
     alert("This is an equilateral triangle because all the sides are equal");
   }
   else if(side1===side2||side2===side3||side3===side1){
     alert("This is an Isosceles triangle as exactly two sides are equal");
   }
-  else if(side1!==side2&&side2!==side3){
+  else if((side1+side2)<=side3||(side2+side3)<=side1||(side1+side3)<=side2){
+    alert("This is not a triangle as the sum of two sides is greater than the third side");
+  }
+  else if((side1+side2)>side3||(side2+side3)>side1||(side1+side3)>side2){
     alert("This is a scalene triangle as none of its sides are equal");
   }
+  else
+    (alert("Please enter a valid number"));
 }
